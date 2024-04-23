8989b1cb9637f30cd9b715ff0b0757a6987a8e02
jozsua smith <jozsua901@gmail.com>
2024-04-22 21:21:10 -0400

    added tweaks and messed with the server port

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,6 @@
-//referenced from course work - module 24
+//pull in required files
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
 //get route for retrieving existing notes
@@ -9,12 +9,14 @@ notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+//get route for pulling in details about specific note when its clicked on
+//may not need this for it to work - handled by front end
 notes.get('/:id', (req, res) => {
     console.info(`${req.method} request for single note recieved`);
-    const noteDetail = (req.params.uid);
+    const noteDetail = (req.params.id);
     readFromFile('./db/db.json').then((data) => {
         let noteDetail = res.json(JSON.parse(data));
-        const note = noteDetail.find((note) => note.uid === noteUid);
+        const note = noteDetail.find((note) => note.id === noteId);
         if (note) {
             res.json(note);
         } else {
@@ -27,6 +29,7 @@ notes.get('/:id', (req, res) => {
     });
 });
 
+//post route for adding in new notes
 notes.post('/', (req, res) => {
     console.log(req.body);
 const { title, text } = req.body;
@@ -35,14 +38,33 @@ if (req.body) {
     const newNote = {
         title,
         text,
-        uid: uuid(),
+        id: uuid(),
     };
-
+    //add it to db.json file
     readAndAppend(newNote, 'db/db.json');
     res.json(`Note Added Sucessfully`);
     }  else {
         res.error('Error in adding Note')
     }
 });
+//route to delete the notes from the front end UI
+notes.delete('/:id', (req, res) => {
+    readFromFile('./db/db.json').then((data) => {
+        let noteDetail = JSON.parse(data);
+        console.log("This is the array: ", noteDetail)
+        const noteId = (req.params.id);//important
+        console.log("this is the note that we want to delete :", noteId)
+        //use filter to pull and "keep" all of the notes EXCEPT the one i want deleted!
+        const note = noteDetail.filter((note) => note.id !== noteId);
+        console.log(note);
+        writeToFile('db/db.json', note);
+        res.json(`Note Deleted Sucessfully`);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).json({error: 'Error with server response'})
+    });
+    
+})
 
 module.exports = notes;
